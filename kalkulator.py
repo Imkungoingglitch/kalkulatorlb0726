de2a1ec350a70cd223cec646a70a3adc3dc7868f
unknown <fredlyarthara03@gmail.com>
2026-03-17 15:20:26 +0700

    update lagi

--- kalkulator.py
+++ kalkulator.py
@@ -1,1 +1,57 @@
-#please add your code here
+x = input("masukkan angka dan opr: ")
+x += "+" 
+stack = []
+num = ""
+opr = "+"
+
+for i in x:
+    if i.isdigit():
+        num += i
+    elif i in "+-*/":
+        val = int(num)
+        if opr == "+":
+            stack.append(val)
+        elif opr == "-":
+            stack.append(-val) 
+        elif opr == "*":
+
+            last_val = stack.pop()
+            stack.append(last_val * val)
+        elif opr == "/":
+
+            last_val = stack.pop()
+            stack.append(int(last_val / val))
+        
+        num = ""
+        opr = i
+
+print(f"Isi Stack: {stack}")
+print(f"Hasil Akhir: {sum(stack)}")x = input("masukkan angka dan opr: ")
+x += "+" 
+stack = []
+num = ""
+opr = "+"
+
+for i in x:
+    if i.isdigit():
+        num += i
+    elif i in "+-*/":
+        val = int(num)
+        if opr == "+":
+            stack.append(val)
+        elif opr == "-":
+            stack.append(-val) 
+        elif opr == "*":
+
+            last_val = stack.pop()
+            stack.append(last_val * val)
+        elif opr == "/":
+
+            last_val = stack.pop()
+            stack.append(int(last_val / val))
+        
+        num = ""
+        opr = i
+
+print(f"Isi Stack: {stack}")
+print(f"Hasil Akhir: {sum(stack)}")     
